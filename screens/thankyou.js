4c9ba61d41f671412187ced6ed56affccc4338aa
ddamen <65085331+ddamen@users.noreply.github.com>
2021-03-19 14:25:32 -0400

    Update thankyou.js

--- screens/thankyou.js
+++ screens/thankyou.js
@@ -4,8 +4,31 @@ import { globalStyles } from '../styles/global';
 
 export default function Home() {
   return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>thank you screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.welcomeTitle}>Thank you for participating! </Text> 
+      <Text style={styles.subTitle}>Return again tomorrow to continue.  </Text>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  welcomeTitle: {
+    alignSelf: 'center',
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }, 
+  subTitle1: {
+    alignSelf: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },  
+  container: {
+    flex: 1,
+    backgroundColor: '#fff', 
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
